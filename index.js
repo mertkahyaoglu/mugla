27e1ac9977da6657b3b404878a31346a69f0af1b
mertkahyaoglu <mertkahyaoglu93@gmail.com>
2019-03-18 10:44:55 +0300

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+const mugla = () => [
+  "Bodrum",
+  "Dalaman",
+  "Datça",
+  "Fethiye",
+  "Kavaklıdere",
+  "Köyceğiz",
+  "Marmaris",
+  "Menteşe",
+  "Milas",
+  "Ortaca",
+  "Seydikemer",
+  "Ula",
+  "Yatağan"
+];
+
+module.exports = mugla;
---
+++ readme.md
@@ -0,0 +1,3 @@
+# mugla
+
+> mugla
